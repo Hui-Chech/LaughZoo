995e10a0cae6b7ad87ac17457b455afc1041c0c0
HUI-CHECH Chang <huichechchang@gmail.com.tw>
2024-01-27 17:22:37 +0800

    [更新] 更新物件管理腳本

--- LaughZoo/Assets/ExtraScript/SpriteManager.cs
+++ LaughZoo/Assets/ExtraScript/SpriteManager.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpritePartLibrary", menuName = "New SpritePartLibrary", order = 1)]
 public class SpriteManager : ScriptableObject
 {
-    public List<SpriteList> FaceParts = new List<SpriteList>();
+    public SpriteList faceParts = new SpriteList();
 }
 
 [System.Serializable]
 public class SpriteList
 {
-    public string AnimalName;
-    public List<Sprite> EyeLeft = new List<Sprite>();
-    public List<Sprite> EyeRight = new List<Sprite>();
-    public List<Sprite> Nose = new List<Sprite>();
-    public List<Sprite> Mouth = new List<Sprite>();
+    public string Name;
+    public List<Sprite> EyeLeft;
+    public List<Sprite> EyeRight;
+    public List<Sprite> Nose;
+    public List<Sprite> Mouth;
 }
