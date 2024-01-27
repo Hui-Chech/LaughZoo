d4497e68baf4ab7674ca4888825b3e189c77c6a5
HUI-CHECH Chang <huichechchang@gmail.com.tw>
2024-01-28 01:35:29 +0800

    [新增] 新增BGMSystem Package

---
+++ LaughZoo/Assets/BGMSystem/BGMSystem.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMSystem : MonoBehaviour
+{
+    static BGMSystem instance;
+
+    AudioSource audio;
+
+    void OnDestroy()
+    {
+        instance = null;
+    }
+
+    static void Initial()
+    {
+        if (instance != null) return;
+
+        GameObject instanceObject = new GameObject("BGM System");
+        DontDestroyOnLoad(instanceObject);
+
+        instance = instanceObject.AddComponent<BGMSystem>();
+
+
+        instance.audio = instanceObject.AddComponent<AudioSource>();
+        instance.audio.loop = true;
+    }
+    internal static void SetMusic(AudioClip music)
+    {
+        Initial();
+
+        instance.Instance_SetMusic(music);
+    }
+    public static void SetSpeed(float speed)
+    {
+        Initial();
+
+        instance.Instance_SetSpeed(speed);
+    }
+
+    void Instance_SetMusic(AudioClip music, bool reset = true)
+    {
+        audio.clip = music;
+        audio.Play();
+
+        if(reset)
+        {
+            Instance_SetSpeed(1);
+        }
+    }
+    void Instance_SetSpeed(float speed)
+    {
+        audio.pitch = speed;
+    }
+}
