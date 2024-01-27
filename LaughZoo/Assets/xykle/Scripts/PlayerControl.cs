7771312bfb5607329a9184649f7c561d2949ca1d
xykle <happy30102@gmail.com>
2024-01-27 16:34:52 +0800

    [新增] 基礎顏面印章功能

---
+++ LaughZoo/Assets/xykle/Scripts/PlayerControl.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+public class PlayerControl : MonoBehaviour
+{
+    #region Singleton
+    public static PlayerControl Instance;
+
+    private void SingletonInit()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    [Header("Juicy Settings")]
+    [SerializeField] private float sizeOnStamp;
+    [SerializeField] private float stampDuration;
+    [SerializeField] private LeanTweenType type;
+
+    public bool isGameStarted = false;
+    private Camera mainCam;
+    private GameObject spriteObject;
+    private Vector3 originalSpriteSize;
+    private Collider2D collider;
+
+    private void Awake()
+    {
+        SingletonInit();
+
+        Init();
+    }
+
+    private void Init()
+    {
+        mainCam = Camera.main;
+        spriteObject = transform.GetChild(0).gameObject;
+        collider = GetComponent<Collider2D>();
+
+        originalSpriteSize = spriteObject.transform.localScale;
+        collider.enabled = false;
+    }
+
+    void Update()
+    {
+        if (!isGameStarted) return;
+
+        FaceMovement();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StampOnFace();
+        }
+    }
+
+    // Player face movement control
+    private void FaceMovement()
+    {
+        var mousePosition = Input.mousePosition;
+        mousePosition.z = Mathf.Abs(mainCam.transform.position.z);
+        transform.position = mainCam.ScreenToWorldPoint(mousePosition);
+    }
+
+    // Player stamp control
+    private void StampOnFace()
+    {
+        LeanTween.cancel(gameObject);
+        LeanTween.cancel(spriteObject);
+
+        collider.enabled = true;
+        LeanTween.delayedCall(gameObject, stampDuration, () => collider.enabled = false);
+
+        // Juicy effect
+        spriteObject.transform.localScale = originalSpriteSize;
+        LeanTween.scale(spriteObject, originalSpriteSize * sizeOnStamp, stampDuration).setEase(type).setLoopPingPong(1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.parent = spriteObject.transform;
+        collision.GetComponent<FlyingSprite>().enabled = false;
+    }
+}
