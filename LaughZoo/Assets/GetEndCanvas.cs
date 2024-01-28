673f65e19b535c9de2232c78277481c40926c1f3
HUI-CHECH Chang <huichechchang@gmail.com.tw>
2024-01-28 13:47:14 +0800

    [更新] 取得結算畫面腳本

---
+++ LaughZoo/Assets/GetEndCanvas.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetEndCanvas : MonoBehaviour
+{
+    [SerializeField] private Transform finshGroupRoot;
+
+    public Transform GetFinshGroupRoot()
+    {
+        return finshGroupRoot;
+    }
+}
