a1bf2509d5329334d4e7618809b0f3f2c98f9c1e
HUI-CHECH Chang <huichechchang@gmail.com.tw>
2024-01-28 11:22:54 +0800

    [修改] 修改生成物件腳本，傳入Type指定該物件為何種部件

--- LaughZoo/Assets/ExtraScript/Step/InstanceSpriteStep.cs
+++ LaughZoo/Assets/ExtraScript/Step/InstanceSpriteStep.cs
@@ -9,6 +9,7 @@ public class InstanceSpriteStep : Step
 
     float probability;
     bool isFinsh;
+    PartType type;
 
     /// <summary>
     /// 生成圖片步驟
@@ -16,18 +17,23 @@ public class InstanceSpriteStep : Step
     /// <param name="sprite"></param>
     /// <param name="probability"></param>
     /// <param name="parent"></param>
-    public InstanceSpriteStep(Sprite sprite, float probability, Transform parent)
+    public InstanceSpriteStep(Sprite sprite, float probability, Transform parent, PartType partType)
     {
         this.sprite = sprite;
         this.probability = probability;
         this.parentTrs = parent;
+        this.type = partType;
     }
     public override void OnEnter()
     {
         GameObject faceSpritePartParent = new GameObject("FaceSpritePartParent");
 
         var facePart = InstanceFaceSprite.InstanceSprite(sprite, probability, faceSpritePartParent.transform);
-        faceSpritePartParent.AddComponent<RandomFly>();
+
+        var facePartBase = faceSpritePartParent.AddComponent<FacePartBase>();
+        facePartBase.type = type;
+
+        isFinsh = true;
     }
 
     public override void OnUpdate()
